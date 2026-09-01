debfbf706aa3f27728e03bb346f3fdf5d2a11ecc
Brenda Cristaldo <112977198+brendacristaldo@users.noreply.github.com>
2026-09-01 12:02:19 -0300

    feat: configura conexão do backend com PostgreSQL
    Adiciona configuração do banco de dados com dotenv e pg, cria conexão por meio de Pool e implementa rota para validação da comunicação com o PostgreSQL.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./src/config/database');
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,23 @@ app.get('/', (req, res) => {
     res.json({
         mensagem: 'API do sistema de gestão financeira rodando.'
     });
+});
+
+app.get('/teste-banco', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT NOW()');
+
+        res.json({
+            mensagem: 'Conexão com PostgreSQL funcionando!',
+            horario: resultado.rows[0].now
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            mensagem: 'Erro ao conectar com o PostgreSQL.'
+        });
+    }
 });
 
 app.listen(PORT, () => {
